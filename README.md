8a3b328ca66da21bd64cfdab735747aab044be8e
anacardo <pedro.fortunato.89@gmail.com>
2024-02-28 21:15:18 +0000

    toDew - first iteration

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# todoApp
-todoApp frontend
+# ToDew - ToDo for DumDums
+
+**ToDew is ToDo for DumDums:**
+1. shainy colors
+2. drag'n'drop ToDews
+3. xek off ToDews
+4. fil gud 4 gud werk
+
+## Ok fr fr now:
+I used this as a base to better unsderstand event-handling as an opportunity to remind myself to appretiate jQuery just... for existing.
+
+### Install:
+- No install needed, just clone the repo and run index.html
+
+### Usage:
+- the blue button saves an image of the boards
+- the red button clears the working copy of the boards
+
+For hygiene purposes the creator strongly advises steering clear of the css file as it's a cesspool, but then again what css file isn't AMIRITE!? Heh...heh... \*weep\*
---
+++ ToDew/app.js
@@ -0,0 +1,201 @@
+// on load
+const inputBox = document.getElementById("task-input");
+const todoList = document.getElementById("todo-list");
+const workingList = document.getElementById("working-list");
+const pendingList = document.getElementById("pending-list");
+const testingList = document.getElementById("testing-list");
+const doneList = document.getElementById("done-list");
+let draggedItem = null;
+
+showData();
+addContainerListeners();
+addItemListeners();
+
+
+
+// logic
+function addTask() {
+  if (inputBox.value === '') {
+    alert("Cannot add what there isn't");
+    return;
+  } else {
+    let taskWrapper = document.createElement("div");
+    taskWrapper.classList.add('draggable-item')
+    taskWrapper.setAttribute('draggable', 'true');
+    taskWrapper.classList.add('no-decor')
+    let task = makeTask(inputBox.value);
+    taskWrapper.appendChild(task);
+    todoList.appendChild(taskWrapper);
+  }
+  inputBox.value = '';
+  addItemListeners();
+}
+
+function itemToForm(e) {
+  let div = e.target.parentElement.parentElement;
+  div.classList.remove('draggable-item');
+  div.classList.add('editable-item');
+  let p = e.target.parentElement;
+  let taskText = p.textContent;
+  let textToAdd = taskText.slice(0, -2)
+  p.remove();
+  let form = makeTaskEdit(textToAdd)
+  div.appendChild(form);
+}
+
+function confirmEdit() {
+  let divs = document.getElementsByClassName('editable-item');
+  let taskToAdd = null;
+  for (const div of divs) {
+    if (div.children[0].children[0].value === '') {
+      alert("ToDew nid name");
+      return;
+    }  else {
+      taskToAdd = makeTask(div.children[0].children[0].value);
+      div.innerHTML = '';
+    }
+    for (const div of divs) {
+      div.appendChild(taskToAdd);
+      div.classList.remove('editable-item');
+      div.classList.add('draggable-item');
+    }
+  }
+}
+
+function makeTask(text) {
+  let p = document.createElement("p");
+  let spanEdit = document.createElement("span");
+  let spanX = document.createElement("span");
+  p.innerHTML = text;
+  spanEdit.innerHTML = '\u25a0';
+  spanEdit.classList.add('edit');
+  spanX.innerHTML = '\u00d7';
+  spanX.classList.add('delete');
+  p.appendChild(spanEdit);
+  p.appendChild(spanX);
+  return p
+}
+
+function makeTaskEdit(text) {
+  const input = document.createElement("input");
+  const button = document.createElement("button");
+  input.setAttribute('type', 'text');
+  input.setAttribute('value', text);
+  button.classList.add('submit-edit');
+  button.textContent = "Ye";
+  button.setAttribute('onclick', "confirmEdit(); return false;")
+  const form = document.createElement("form");
+  form.appendChild(input);
+  form.appendChild(button);
+  return form
+}
+
+// event listeners
+function addItemListeners() {
+  const draggableItems = document.querySelectorAll('.draggable-item');
+  draggableItems.forEach(function(draggableItem) {
+    draggableItem.addEventListener('dragstart', handleDragStart);
+    draggableItem.addEventListener('dragend', handleDragEnd);
+  });
+}
+
+function addContainerListeners() {
+  const droppableLists = document.querySelectorAll('.droppable-list');
+  droppableLists.forEach(function(droppableList) {
+    droppableList.addEventListener('dragover', handleDragOver);
+    droppableList.addEventListener('dragenter', handleDragEnter);
+    droppableList.addEventListener('dragleave', handleDragLeave);
+    droppableList.addEventListener('drop', handleDrop);
+    droppableList.addEventListener('click', handleClick);
+  });
+}
+
+// event handlers
+function handleDragStart() {
+  draggedItem = this;
+  this.classList.add('dragging');
+}
+  
+function handleDragEnd() {
+  draggedItem = null;
+  this.classList.remove('dragging');
+}
+
+function handleDragOver(e) {
+  e.preventDefault();
+  this.classList.add('dragover');
+}
+
+function handleDragLeave() {
+  this.classList.remove('dragover');
+}
+
+function handleDragEnter(e) {
+  e.preventDefault();
+}
+
+function handleClick(e) {
+  if (e.target.matches('.delete')) {
+    e.target.parentElement.parentElement.remove();
+  } else if (e.target.matches('.edit')) {
+    itemToForm(e);
+  } else if (e.target.matches('.draggable-item')) {
+    e.target.classList.toggle('check');
+  } else if (e.target.parentElement.matches('.draggable-item')) {
+    e.target.parentElement.classList.toggle('check');
+  }
+  // let itemState = ['no-decor', 'wait', 'check', 'warn']
+  // for (let i = 0; i < itemState.length; i++) {
+  //   if (e.target.classlist.contains(itemState[i])) {
+  //     if (i === length(itemState-1)) {
+  //       e.target.classList.add(itemState[0])
+  //     }
+  //     e.target.classList.add(itemState[i+1])
+  //     e.target.classList.remove(itemState[i])
+  //     return;
+  //   } else if (e.target.parentElement.classList.contains(itemState[i])) {
+  //     if (i === length(itemState-1)) {
+  //       e.target.parentElement.classList.add(itemState[0])
+  //     }
+  //     e.target.parentElement.classList.add(itemState[i+1])
+  //     e.target.parentElement.classList.remove(itemState[i])
+  //     return;
+  //   }
+  // }  
+}
+
+function handleDrop(e) {
+  e.preventDefault();
+  let target = e.target;
+  if (target.classList.contains('draggable-item')) {
+    target.parentNode.insertBefore(draggedItem, target);
+  } else if (target.classList.contains('droppable-list')) {
+    this.appendChild(draggedItem);
+  }
+  this.classList.remove('dragover');
+}
+
+  // storage
+function saveData() {
+  localStorage.setItem('tasks-todo', todoList.innerHTML);
+  localStorage.setItem('tasks-working', workingList.innerHTML);
+  localStorage.setItem('tasks-pending', pendingList.innerHTML);
+  localStorage.setItem('tasks-testing', testingList.innerHTML);
+  localStorage.setItem('tasks-done', doneList.innerHTML);
+}
+
+function showData() {
+  todoList.innerHTML = localStorage.getItem('tasks-todo');
+  workingList.innerHTML = localStorage.getItem('tasks-working');
+  pendingList.innerHTML = localStorage.getItem('tasks-pending');
+  testingList.innerHTML = localStorage.getItem('tasks-testing');
+  doneList.innerHTML = localStorage.getItem('tasks-done');
+}
+
+function clearData() {
+  todoList.innerHTML = '';
+  workingList.innerHTML = '';
+  pendingList.innerHTML = '';
+  testingList.innerHTML = '';
+  doneList.innerHTML = '';
+}
